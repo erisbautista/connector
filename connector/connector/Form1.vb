e494a0cc478253a341dd28791fb717d37fc81799
erisbautista <bautistaerielle@gmail.com>
2018-06-06 11:03:36 +0800

    updated the codes to make connection successful

--- connector/connector/Form1.vb
+++ connector/connector/Form1.vb
@@ -1,8 +1,7 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim mysqlconn = New MySqlConnection("server=127.0.0.1; database=Database; port=3306")
-
+        Dim mysqlconn = New MySqlConnection("server=localhost; user=root; database=7/11 store; port=3306; SslMode=none")
         Try
             mysqlconn.Open()
             MessageBox.Show("Connection Successful")
